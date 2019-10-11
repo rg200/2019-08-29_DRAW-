ca133a1740b0ab057e88c6a238f86efb0a7778f8
jongpyo <jongpyo@175.121.156.232>
2019-10-11 21:59:41 +0900

    10/11 종표 클라 수정

--- DRAW/src/Lobby_Screen/Lobby_Background.java
+++ DRAW/src/Lobby_Screen/Lobby_Background.java
@@ -1,11 +1,15 @@
 package Lobby_Screen;
-import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -32,8 +36,18 @@ public class Lobby_Background extends JPanel{
 	private JTextArea Chat_TextArea = new JTextArea();
 	private JScrollPane Chat_TextArea_Scroll = new JScrollPane(Chat_TextArea);
 	
-	
-	public Lobby_Background(Default_Frame DF) {
+	// 네트워크
+		private  Socket clientSocket = null;// 클라이언트 소켓
+		private  DataInputStream inData = null;// 데이터 인풋
+		private  DataOutputStream outData = null; // 데이터 아웃풋
+		private Socket Chat = null;//	채팅 소켓
+		private DataInputStream inChat= null;//Receive에 필요함
+		private DataOutputStream outChat= null;
+		private Socket RoomInfo= null;//	방 정보 소켓
+		private DataInputStream inRoomInfo= null;// 	요청할 때에 필요함
+		private DataOutputStream outRoomInfo= null;//응답할 때 필요
+		
+	public Lobby_Background(Default_Frame DF,Socket clientSocket) {
 		this.DF = DF;
 		setSize(Default_Frame.SCREEN_WIDTH, Default_Frame.SCREEN_HEIGHT);// 크기 고정
 		setLayout(null); 
@@ -41,7 +55,6 @@ public class Lobby_Background extends JPanel{
 		add(LOBBY_BUTTON);	
 		add(LOBBY_BACKBUTTON);	
 		add(Chat_TextField);
-		//add(Chat_TextArea);
 		add(Chat_TextArea_Scroll);
 		Chat_TextArea_Scroll.setVisible(true);
 
@@ -82,57 +95,28 @@ public class Lobby_Background extends JPanel{
 		LOBBY_BUTTON.setContentAreaFilled(false);
 		LOBBY_BUTTON.setBounds(1235,810, LOBBY_BACK.getIconWidth(), LOBBY_BACK.getIconHeight());
 		
-		// 네트워크
-		private  Socket clientSocket = null;// 클라이언트 소켓
-		private  DataInputStream inData = null;// 데이터 인풋
-		private  DataOutputStream outData = null; // 데이터 아웃풋
-		private Socket Chat = null;//	채팅 소켓
-		private DataInputStream inChat= null;//Receive에 필요함
-		private DataOutputStream outChat= null;
-		private Socket RoomInfo= null;//	방 정보 소켓
-		private DataInputStream inRoomInfo= null;// 	요청할 때에 필요함
-		private DataOutputStream outRoomInfo= null;//응답할 때 필요
+		// 네트워크 부분 시작
+		// 기본 호스트(자기 자신)
+		String host = "localhost";
 
-		public Lobby_Background(Default_Frame DF, Socket clientSocket) {
-			this.DF = DF;
-			setSize(Default_Frame.SCREEN_WIDTH, Default_Frame.SCREEN_HEIGHT);// 크기 고정
-			setLayout(null);
+		try {
+			Chat = new Socket(host, 9001);
+			outChat = new DataOutputStream(Chat.getOutputStream());
+			inData = new DataInputStream(Chat.getInputStream());
+			System.out.println("채팅서버 접속 성공");
 
-			add(LOBBY_BUTTON);
-			add(LOBBY_BACKBUTTON);
-			LOBBY_BACKBUTTON.setBorderPainted(false);
-			LOBBY_BACKBUTTON.setFocusable(false);
-			LOBBY_BACKBUTTON.setContentAreaFilled(false);
-			LOBBY_BACKBUTTON.setBounds(1235, 925, LOBBY_BACK.getIconWidth(), LOBBY_BACK.getIconHeight());
+			RoomInfo = new Socket(host, 9002);
+			outRoomInfo = new DataOutputStream(RoomInfo.getOutputStream());
+			inRoomInfo = new DataInputStream(RoomInfo.getInputStream());
+			System.out.println("룸인포서버 접속 성공");
+		} catch (UnknownHostException e) {
+			System.err.println("호스트를 찾을 수 없습니다." + host);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to the host " + host);
+		}
 
-			LOBBY_BUTTON.setBorderPainted(false);
-			LOBBY_BUTTON.setFocusable(false);
-			LOBBY_BUTTON.setContentAreaFilled(false);
-			LOBBY_BUTTON.setBounds(1235, 810, LOBBY_BACK.getIconWidth(), LOBBY_BACK.getIconHeight());
+		// 네트워크 부분 끝
 
-			
-			// 네트워크 부분 시작
-					// 기본 호스트(자기 자신)
-					String host = "localhost";
-
-					try {
-						Chat = new Socket(host, 9001);
-						outChat = new DataOutputStream(Chat.getOutputStream());
-						inData = new DataInputStream(Chat.getInputStream());
-						System.out.println("채팅서버 접속 성공");
-						
-						RoomInfo = new Socket(host, 9002);
-						outRoomInfo = new DataOutputStream(RoomInfo.getOutputStream());
-						inRoomInfo = new DataInputStream(RoomInfo.getInputStream());
-						System.out.println("룸인포서버 접속 성공");
-					} catch (UnknownHostException e) {
-						System.err.println("호스트를 찾을 수 없습니다." + host);
-					} catch (IOException e) {
-						System.err.println("Couldn't get I/O for the connection to the host " + host);
-					}
-
-					// 네트워크 부분 끝
-			
 			
 		
 		LOBBY_BUTTON.addMouseListener(new MouseAdapter() {
