b01c9a46e9bbf3698bac86fbd9ee458deebddd40
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2019-11-02 19:18:43 +0900

    Update Play_List.java

--- DRAW/src/Lobby_Screen/Play_List.java
+++ DRAW/src/Lobby_Screen/Play_List.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
 public class Play_List extends JPanel {
-	
+	// 방목록이 표시되는 패널을 나타넴 (영역이라고 생각하면 편함)
 	public Play_List() {
 		setLayout(new FlowLayout(FlowLayout.LEFT)); 
 		setOpaque(false);
