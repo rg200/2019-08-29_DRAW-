6b1d24ecd6d26dd296ec03d2fa212e10178f9558
hanapple07 <54665408+hanapple07@users.noreply.github.com>
2019-11-02 19:05:58 +0900

    Update Signup_Button.java

--- DRAW/src/Signup_Screen/Signup_Button.java
+++ DRAW/src/Signup_Screen/Signup_Button.java
@@ -10,6 +10,7 @@ import Default.Default_Frame;
 import Main_Screen.Main_Background;
 
 public class Signup_Button extends Default_Button_Event{
+	// íšŒì› ê°€ì…ì„ ì‹œí‚¤ëŠ” ë²„íŠ¼ 
 	private ImageIcon SIGNUP_IN = new ImageIcon(Main_Background.class.getResource("/Image/SIGNUP_SCREEN/SIGNUP_IN.png"));
 	private ImageIcon SIGNUP_ENTER = new ImageIcon(Main_Background.class.getResource("/Image/SIGNUP_SCREEN/SIGNUP_ENTER.png"));
 	private Default_Frame DF;
@@ -25,14 +26,14 @@ public class Signup_Button extends Default_Button_Event{
 	}
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		setIcon(SIGNUP_ENTER);//Èò»öÀ¸·Î ¹Ù²ñ
-		setCursor(new Cursor(Cursor.HAND_CURSOR));// Ä¿¼­°¡ ¼Õ°¡¶ô ¸ğ¾çÀ¸·Î
+		setIcon(SIGNUP_ENTER);//í°ìƒ‰ìœ¼ë¡œ ë°”ë€œ
+		setCursor(new Cursor(Cursor.HAND_CURSOR));// ì»¤ì„œê°€ ì†ê°€ë½ ëª¨ì–‘ìœ¼ë¡œ
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		setIcon(SIGNUP_IN);// ´Ù½Ã ¿ø»óÅÂ·Î
-		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));// Ä¿¼­°¡ ±âº»À¸·Î
+		setIcon(SIGNUP_IN);// ë‹¤ì‹œ ì›ìƒíƒœë¡œ
+		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));// ì»¤ì„œê°€ ê¸°ë³¸ìœ¼ë¡œ
 	}
 
 	@Override
