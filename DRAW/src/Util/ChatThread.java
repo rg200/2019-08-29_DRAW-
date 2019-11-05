558c3b2cebf813cb3ab7b5631980b729d273a63f
jpoh281 <54665433+jpoh281@users.noreply.github.com>
2019-11-05 14:42:59 +0900

    Update ChatThread.java

--- DRAW/src/Util/ChatThread.java
+++ DRAW/src/Util/ChatThread.java
@@ -1,15 +1,16 @@
 package Util;
 
+import java.awt.Color;
+
 import javax.swing.JTextArea;
 
-import Default.Default_Frame;
 import Default.Default_Socket;
 
 
 public class ChatThread implements Runnable {
 	private static JTextArea Chat_TextArea;
 	private String text;
-	
+	private Color green = Color.GREEN;
 	public ChatThread() {
 		new Thread(this).start();
 		this.text = null;
@@ -27,6 +28,9 @@ public class ChatThread implements Runnable {
 			text = ReceiveServer.ReceiveData(Default_Socket.getInChat());
 			if(text.equals("Logout")) {
 				break;
+			}
+			else if(text.startsWith("SYSW ")) {
+				text = text.substring(5); //SYSW 를 지우기 위해서 사용
 			}
 			Chat_TextArea.append(text + "\n");
 			Chat_TextArea.setCaretPosition(Chat_TextArea.getDocument().getLength());
